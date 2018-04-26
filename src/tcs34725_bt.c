299353a4320e79f229a5970724a720ab01d9c470
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-04-26 11:06:34 +0300

    tcs34725_bt: Fix indentation

--- src/tcs34725_bt.c
+++ src/tcs34725_bt.c
@@ -11,20 +11,20 @@ struct device *dev;
 
 void tcs34725_bt_init(void)
 {
-    dev = device_get_binding("TCS34725");
-    if (!dev) {
-        SYS_LOG_ERR("Failed to get TCS34725H device binding");
-        return;
-    }
+	dev = device_get_binding("TCS34725");
+	if (!dev) {
+		SYS_LOG_ERR("Failed to get TCS34725H device binding");
+		return;
+	}
 }
 
 void tcs34725_bt_update(void)
 {
-    struct sensor_value r, g, b, l;
-    int err;
+	struct sensor_value r, g, b, l;
+	int err;
 
-    if (!dev)
-        return;
+	if (!dev)
+		return;
 
 	err = sensor_sample_fetch(dev);
 	if (err)
@@ -34,10 +34,12 @@ void tcs34725_bt_update(void)
 	sensor_channel_get(dev, SENSOR_CHAN_GREEN, &g);
 	sensor_channel_get(dev, SENSOR_CHAN_BLUE, &b);
 	sensor_channel_get(dev, SENSOR_CHAN_LIGHT, &l);
-    
-    tcs34725_bt_r = r.val1;
-    tcs34725_bt_g = g.val1;
-    tcs34725_bt_b = b.val1;
-    tcs34725_bt_l = l.val1;
-    printf("yeeeeeeeeeeeeeeeeee\n");
+
+	tcs34725_bt_r = r.val1;
+	tcs34725_bt_g = g.val1;
+	tcs34725_bt_b = b.val1;
+	tcs34725_bt_l = l.val1;
+
+	SYS_LOG_INF("r: %d, r: %d, b: %d, l: %d\n", r.val1, g.val1, b.val1,
+		    l.val1);
 }
